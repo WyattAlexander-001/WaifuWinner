e5011c897b0dc018ce4c3a7c83900be119fe002e
WyattAlexander-001 <wyattbushman117@gmail.com>
2025-01-18 16:10:53 -0500

    added projectiles

--- waifu_winner/player/player.gd
+++ waifu_winner/player/player.gd
@@ -5,6 +5,26 @@ extends CharacterBody2D
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var walk_timer: Timer = $walk_timer
 
+# Attacks
+var ice_spear = preload("res://player/attack/ice_spear.tscn")
+
+# Attack Nodes:
+@onready var ice_spear_timer = get_node("%IceSpearTimer")
+@onready var ice_spear_attack_timer = get_node("%IceSpearAttackTimer")
+
+# IceSpear
+var ice_spear_ammo = 0
+var ice_spear_base_ammo = 3
+var ice_spear_attack_speed = 1.5
+var ice_spear_level = 1
+
+# Enemy Related
+var enemy_close = []
+
+
+func _ready():
+	attack()
+
 func _physics_process(delta: float) -> void:
 	movement()
 	
@@ -31,6 +51,46 @@ func movement():
 	move_and_slide()
 	
 	
+	
 func _on_hurt_box_hurt(damage: Variant) -> void:
 	hp -= damage
 	print("Player HP Current Value: " + str(hp))
+	
+func attack():
+	if ice_spear_level > 0:
+		ice_spear_timer.wait_time = ice_spear_attack_speed
+		if ice_spear_timer.is_stopped():
+			ice_spear_timer.start()
+
+func _on_ice_spear_timer_timeout() -> void:
+	ice_spear_ammo += ice_spear_base_ammo
+	ice_spear_attack_timer.start()
+
+func _on_ice_spear_attack_timer_timeout() -> void:
+	if ice_spear_ammo > 0:
+		var ice_spear_attack = ice_spear.instantiate()
+		ice_spear_attack.position = position
+		ice_spear_attack.target = get_random_target()
+		ice_spear_attack.level = ice_spear_level
+		add_child(ice_spear_attack)
+		ice_spear_ammo -= 1
+		if ice_spear_ammo > 0:
+			ice_spear_attack_timer.start()
+		else:
+			ice_spear_attack_timer.stop()
+		
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body: Node2D) -> void:
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body: Node2D) -> void:
+	if enemy_close.has(body):
+		enemy_close.erase(body)
